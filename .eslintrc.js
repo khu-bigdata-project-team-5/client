eff02fc8c729b6a0eb9f7ad5022d45845de28a48
milkbottle0305 <bigtog0602@gmail.com>
2024-05-31 22:43:26 +0900

    chore: Lint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  settings: {
+    react: {
+      version: "detect", // React 버전을 자동으로 감지하도록 설정
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/prop-types": "off",
+
+    // 다른 규칙들...
+  },
+};
