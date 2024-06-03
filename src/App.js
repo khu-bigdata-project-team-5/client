91ccba531646f13daf2b7eaa24ba162b8fa7caea
milkbottle0305 <bigtog0602@gmail.com>
2024-06-03 22:12:12 +0900

    feat: 컴포넌트 임포트 및 일부 컴포넌트명 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home/Home";
+import Lecture from "./pages/Lecture/Lecture";
+import CurriculumCategory from "./pages/CurriculumCategory/CurriculumCategory";
+import CurriculumKeyword from "./pages/CurriculumKeyword/CurriculumKeyword";
+import Curriculum from "./pages/Curriculum/Curriculum";
+import Another from "./pages/Another/Another";
+import AnotherCurriculum from "./pages/AnotherCurriculum/AnotherCurriculum";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
   return (
     <Routes>
@@ -9,8 +18,8 @@ function App() {
       <Route path="/curriculum/category" element={<CurriculumCategory />} />
       <Route path="/curriculum/keyworld" element={<CurriculumKeyword />} />
       <Route path="/curriculum" element={<Curriculum />} />
-      <Route path="/another" element={<AnotherCurriculums />} />
-      <Route path="/another/:id" element={<AnotherCurriculumDetail />} />
+      <Route path="/another" element={<Another />} />
+      <Route path="/another/:id" element={<AnotherCurriculum />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
