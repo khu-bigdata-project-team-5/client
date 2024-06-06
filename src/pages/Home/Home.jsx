712f005245a058c133c9d4d9b444c43953354812
milkbottle0305 <bigtog0602@gmail.com>
2024-06-06 16:30:01 +0900

    feat: 데이터 모킹 및 api 연동 구조 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,24 @@
 import * as S from "./Home.style.js";
-import Navbar from "../../componenets/Navbar/Navbar";
+import Loading from "../Loading/Loading.jsx";
+import Navbar from "../../componenets/Navbar/Navbar.jsx";
 import LectureList from "../../componenets/LectureList/LectureList.jsx";
+import useLectures from "../../hooks/useLectures.js";
 
 const Home = () => {
+  const { lectures, loading, error } = useLectures();
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    alert("에러가 발생했습니다.");
+  }
   return (
     <S.HomeLayout>
       <S.HomeHeader>
         <Navbar />
       </S.HomeHeader>
       <S.HomeMain>
-        <LectureList />
+        <LectureList lectures={lectures} />
       </S.HomeMain>
     </S.HomeLayout>
   );
