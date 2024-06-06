f77186d7b3855d445a5cc389decabc5333f6e2bf
milkbottle0305 <bigtog0602@gmail.com>
2024-06-06 21:09:51 +0900

    design: 실시간 키워드 슬라이더 적용

--- src/componenets/Navbar/Navbar.jsx
+++ src/componenets/Navbar/Navbar.jsx
@@ -1,17 +1,32 @@
 import * as S from "./Navbar.style";
 
-const Navbar = () => {
+const Navbar = ({ keywords }) => {
+  const settings = {
+    arrows: false,
+    draggable: false,
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
   return (
     <S.NavbarConatiner>
       <S.NavbarIcon
         src={require("../../assets/icons/logo_nav.png")}
         alt="logo"
       />
-      <S.NavbarRank>
-        실시간 키워드
-        <br />
-        1. Spring Boot
-      </S.NavbarRank>
+      <S.NavbarRankWrapper>
+        <S.NavbarRankTitle>실시간 키워드</S.NavbarRankTitle>
+        <S.NavbarSlider {...settings}>
+          {keywords.map((keyword) => (
+            <S.NavbarRankKeywordItem key={keyword.rank}>
+              {keyword.rank}위 {keyword.keyword}
+            </S.NavbarRankKeywordItem>
+          ))}
+        </S.NavbarSlider>
+      </S.NavbarRankWrapper>
       <S.NavbarItemWrapper>
         <S.NavbarItem>다른 커리큘럼</S.NavbarItem>
         <S.NavbarItem>나의 커리큘럼</S.NavbarItem>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,20 +2,27 @@ import * as S from "./Home.style.js";
 import Loading from "../Loading/Loading.jsx";
 import Navbar from "../../componenets/Navbar/Navbar.jsx";
 import LectureList from "../../componenets/LectureList/LectureList.jsx";
-import useLectures from "../../hooks/useLectures.js";
+import useFetch from "../../hooks/useFetch.js";
+import { ENDPOINTS } from "../../api/endpoints.js";
 
 const Home = () => {
-  const { lectures, loading, error } = useLectures();
-  if (loading) {
-    return <Loading />;
-  }
-  if (error) {
-    alert("에러가 발생했습니다.");
-  }
+  const {
+    data: keywords,
+    loading: keywordsLoading,
+    error: keywordsError,
+  } = useFetch(ENDPOINTS.KEYWORDS);
+  const {
+    data: lectures,
+    loading: lecturesLoading,
+    error: lecturesError,
+  } = useFetch(ENDPOINTS.LECTURES);
+
+  if (keywordsLoading || lecturesLoading) return <Loading />;
+  if (keywordsError || lecturesError) alert("에러가 발생했습니다.");
   return (
     <S.HomeLayout>
       <S.HomeHeader>
-        <Navbar />
+        <Navbar keywords={keywords} />
       </S.HomeHeader>
       <S.HomeMain>
         <LectureList lectures={lectures} />
